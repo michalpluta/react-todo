33da2158ebf47c9d26af7652a9ac66bebbdebb04
Michal <michal@MacBook-Michal.local>
2016-10-19 12:04:39 +0200

    init

---
+++ source/js/app.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Init from './classes/Init';
+
+
+
+
+ReactDOM.render(<Init />, document.getElementById('app'));
+
---
+++ source/js/classes/Init.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+
+const Init = () => {
+return <div>react test</div>
+}
+
+export default Init;
